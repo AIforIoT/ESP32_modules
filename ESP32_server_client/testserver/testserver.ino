ed62d277a112dcb32c7503e0377f9b20ef33f012
smerca <sergi.mercadelaborda@gmail.com>
2018-11-08 14:01:35 +0100

    Updated testserver to handle new functions

--- ESP32_server_client/testserver/testserver.ino
+++ ESP32_server_client/testserver/testserver.ino
@@ -42,32 +42,59 @@ void loop(){
             String currentLine = "";                // make a String to hold incoming data from the client
             while (client.connected()) {            // loop while the client's connected
                 if (client.available()) {             // if there's bytes to read from the client,
-                char c = client.read();             // read a byte, then
-                Serial.write(c);                    // print it out the serial monitor
-                    if (c == '\n') {                    // if the byte is a newline character
-
-                        // if the current line is blank, you got two newline characters in a row.
-                        // that's the end of the client HTTP request, so send a response:
-                        if (currentLine.length() == 0) {
-                            //Nothing
-                        } else {    // if you got a newline, then clear currentLine:
-                            currentLine = "";
-                        }
-                    } else if (c != '\r') {  // if you got anything else but a carriage return character,
-                        currentLine += c;      // add it to the end of the currentLine
-                    }
+                  char c = client.read();             // read a byte, then
+                  Serial.write(c);                    // print it out the serial monitor
+                }
+                //This ESP32 server only expects HTTP Request:
+                // GET /H HTTP/1.x
+                // GET /L HTTP/1.x
+                if (c != '\r') {  // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
                     // Check to see if the client request was "GET /H" or "GET /L":
                     if (currentLine.endsWith("GET /H")) {
                         digitalWrite(5, HIGH);               // GET /H turns the REALY on
-                    }
-                    if (currentLine.endsWith("GET /L")) {
+                        client.println("HTTP/1.1 200 OK");
+                        client.println();
+                        Serial.println("H request detected");
+
+                    }else if (currentLine.endsWith("GET /L")) {
                         digitalWrite(5, LOW);                // GET /L turns the RELAY off
+                        client.println("HTTP/1.1 200 OK");
+                        client.println();
+                        Serial.println("L request detected");
+
+                    }else if (currentLine.endsWith("GET /data/on")) {
+                        // DATA REQUEST FROM RASPI
+                        client.println("HTTP/1.1 200 OK");
+                        client.println();
+                        Serial.println("DATA request detected");
+                        //DATA CODE GOES HERE...
+
+
+                    }else if (currentLine.endsWith("GET /data/off")) {
+                        // DATA REQUEST FROM RASPI
+                        client.println("HTTP/1.1 200 OK");
+                        client.println();
+                        Serial.println("NODATA request detected");
+                        //NODATA CODE GOES HERE...
+
+                    }else if (currentLine.endsWith("GET /volume")) {
+                        // DATA REQUEST FROM RASPI
+                        client.println("HTTP/1.1 200 OK");
+                        client.println();
+                        Serial.println("VOLUME request detected");
+                        //VOLUME CODE GOES HERE...
+
                     }
+                }else{  //End of first request line , no accepted get requested
+                    //Error 405 Method Not Allowed
+                    client.println("HTTP/1.1 405 Method Not Allowed");
+                    client.println();
                 }
             }
             // close the connection:
             client.stop();
             Serial.println("Client Disconnected.");
         }
-    }
-}//END
+    }//END WHILE
+}//END LOOP
